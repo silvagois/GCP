1d7d5c7b37f5f46d5dd47888387b22cd34268686
Marcos Antonio de Gois Silva <marcossilvagois@gmail.com>
2022-12-27 10:03:53 -0300

    Add ParDo como teste

---
+++ ParDo.py
@@ -0,0 +1,41 @@
+import apache_beam as beam
+
+p1 = beam.Pipeline()
+
+class filtro(beam.DoFn):
+  def process(self,record):
+    if int(record[8]) > 0:
+      return [record]
+
+Tempo_Atrasos = (
+  p1
+  | "Importar Dados Atraso" >> beam.io.ReadFromText("voos_sample.csv", skip_header_lines = 1)  
+  #| "Importar Dados Atraso" >> beam.io.ReadFromText(r"C:\Users\cassi\Google Drive\GCP\Dataflow Course\Meu_Curso\voos_sample.csv", skip_header_lines = 1)
+  | "Separar por Vírgulas Atraso" >> beam.Map(lambda record: record.split(','))
+  | "Pegar voos com atraso" >> beam.ParDo(filtro()) # Foi substituido essa linha pela função 
+  | "Criar par atraso" >> beam.Map(lambda record: (record[4],int(record[8])))
+  | "Somar por key" >> beam.CombinePerKey(sum)
+#  | "Mostrar Resultados" >> beam.Map(print)
+)
+
+Qtd_Atrasos = (
+  p1
+  | "Importar Dados" >> beam.io.ReadFromText("voos_sample.csv", skip_header_lines = 1)
+  #| "Importar Dados" >> beam.io.ReadFromText(r"C:\Users\cassi\Google Drive\GCP\Dataflow Course\Meu_Curso\voos_sample.csv", skip_header_lines = 1)
+  | "Separar por Vírgulas Qtd" >> beam.Map(lambda record: record.split(','))
+  | "Pegar voos com Qtd" >> beam.ParDo(filtro()) # Foi substituido essa linha pela função 
+  | "Criar par Qtd" >> beam.Map(lambda record: (record[4],int(record[8])))
+  | "Contar por key" >> beam.combiners.Count.PerKey()
+#  | "Mostrar Resultados QTD" >> beam.Map(print)
+)
+
+tabela_atrasos = (
+    {'Qtd_Atrasos':Qtd_Atrasos,'Tempo_Atrasos':Tempo_Atrasos} 
+    | "Group By" >> beam.CoGroupByKey()
+    | "Saida GCP Bucket" >> beam.io.WriteToText("gs://raw-etl-gcp/entrada/voos_agrupado.csv")
+    #| beam.Map(print)
+)
+
+p1.run()
+
+
